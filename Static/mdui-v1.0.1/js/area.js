676baa6ca717b1dfe8b81e0e81feb7b9bb074a18
hanasaki-misaki <mirai@lolicon.team>
2023-03-20 14:10:44 +0800

    阉割轮播图

---
+++ Static/mdui-v1.0.1/js/area.js
@@ -0,0 +1,14 @@
+(function ($) {
+    'use strict';
+    $(window).on('resizeEnd', function () {
+        $(".welcome_area, .welcome_slides .single_slide, .single_slide").height($(window).height());
+    });
+
+    $(window).on('resize', function () {
+        if (this.resizeTO) clearTimeout(this.resizeTO);
+        this.resizeTO = setTimeout(function () {
+            $(this).trigger('resizeEnd');
+        }, 300);
+    }).trigger("resize");
+    
+})(jQuery);
